b16b0efff4e474fe445183d4ff132fb713f5ad40
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-03-22 22:33:34 -0400

    Add files via upload

---
+++ smallestORbiggest.cs
@@ -0,0 +1,33 @@
+
+    // March 22 -2021 (14 years now)
+    // Project : Find smallest or biggest number
+    
+using System;
+class HelloWorld {
+  static void Main() {
+    int[] comparenums={9,4,2,6,9,7};
+    
+    Console.WriteLine("Smallest number: "+fSbNum(comparenums,true));
+    Console.WriteLine("Biggest number: "+fSbNum(comparenums,false));
+    
+  }
+  
+  static int fSb_Num(int[] nums,bool f_smallest){ // find smallest or biggest number;
+      int topnum;
+      topnum=nums[0];
+      for(int i=0;i<nums.Length;i++){
+          if(f_smallest){
+              if(nums[i]<topnum){
+                  topnum=nums[i];
+              }
+          }else{
+              if(nums[i]>topnum){
+                  topnum=nums[i];
+              }
+          }
+      }
+      
+      return topnum;
+  }
+  
+}
